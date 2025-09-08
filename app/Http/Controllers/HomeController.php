2e58f8394af59100a116c8ef20db47a1db1d7921
auniulilq <auniuloilqisty354@gmail.com>
2025-09-08 14:43:55 +0700

    add dashboard page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
+use App\Models\Borrows;
 use Illuminate\Http\Request;
+use App\Models\DetailBorrows;
+
 
 class HomeController extends Controller
 {
     public function index()
     {
         return view('admin.dashboard');
+        $totalBooks = Book::count();
+        $totalStock = Book::sum('stock');
+
+        //detail_buku ada tidak buku yang sedang di pinjam,actual_date = null
+
+
+        $borrowedBooks = DetailBorrows::with('book','borrow')->whereHas('borrow',function($q){
+        $q->whereNull('actual_return_date');
+        })->count();
+
+        $returnBooks = Borrows::where('status',0)->Arr::whereNotNull('actual_return_date')->count();
+        $NotReturnBooks = Borrows::where('status',1)->whereNull('actual_return_date')->count();
+
+        $fines = Borrows::with('member')->where('fine','>',0)->get();
+        $totalFines = Borrows::sum('fine');
+
+        return view('admin.dashboard',compact('totalBooks','totalStock','borrowedBooks','returnBooks','notReturnBook','fines','totalFines'));
     }
 }
